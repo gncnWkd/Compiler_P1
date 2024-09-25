445f758b6b857ebb311c44856c5f0665c9ec3330
gncnWkd <hyunchan7764@naver.com>
2024-09-25 17:14:25 +0900

    modify lex dfa, custom code ID dfa

--- loucomp/scan.c
+++ loucomp/scan.c
@@ -250,7 +250,7 @@ TokenType getToken(void)
         break;
 
       case INID:
-        if (!isalpha(c)) { 
+        if (!isalpha(c) || !isdigit(c)) {
           /* backup in the input */
           ungetNextChar();
           save = FALSE;
